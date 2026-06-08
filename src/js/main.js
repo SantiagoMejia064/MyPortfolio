d4a562e1e33e7a44c9420b628d76984e9394e32c
SantiagoMejia064 <mejiasantiago0604@gmail.com>
2026-06-07 23:48:17 -0500

    Header listo con sombra animada con el scroll

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,6 @@
-// Add portfolio-specific JavaScript here.
+const header = document.querySelector('header');
+
+window.addEventListener("scroll",() => {
+
+    header.classList.toggle("scrolled", window.scrollY > 0);
+});
