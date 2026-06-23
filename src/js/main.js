5b27185486c368fc3498f7b70ee3c4826dabe1a0
SantiagoMejia064 <mejiasantiago0604@gmail.com>
2026-06-23 11:34:22 -0500

    Página terminada en versión pc y menú hamburguesa maquetado

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,28 @@
 const header = document.querySelector('header');
 
+const menuBtn = document.getElementById('menu-btn');
+const navLinks = document.getElementById('header-nav-links');
+const links = navLinks.querySelectorAll('a');
+
+menuBtn.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    if(navLinks.classList.contains('active')){
+        menuBtn.innerHTML = '✖';
+        menuBtn.setAttribute('aria-expanded', 'true')
+    } else {
+        menuBtn.innerHTML = '☰';
+        menuBtn.setAttribute('aria-expanded', 'false')
+    }
+});
+
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        navLinks.classList.remove('active');
+        menuBtn.innerHTML = '☰';
+        menuBtn.setAttribute('aria-expanded', 'false')
+    })
+});
+
 window.addEventListener("scroll",() => {
 
     header.classList.toggle("scrolled", window.scrollY > 0);
